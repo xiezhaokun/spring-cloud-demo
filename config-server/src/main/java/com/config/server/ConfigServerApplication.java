c41c60b0ac6dcf8f9566a3324e6b26752a54c18a
xiezhaokun <1016340276@qq.com>
2019-04-05 01:03:54 +0800

    高可用注册中心

--- config-server/src/main/java/com/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/config/server/ConfigServerApplication.java
@@ -3,9 +3,15 @@ package com.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableConfigServer
+
+/**
+ * @author xiezhaokun
+ */
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
